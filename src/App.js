ee0502cfe0b7127f18d5448c3a94b7b724bc5483
AVISHEK JANA <avishek@geogo.in>
2021-03-18 16:13:01 +0530

    API implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Detector } from "react-detect-offline";
+
 import './App.css';
 
 function App() {
+  // const localData = JSON.parse(localStorage.getItem('stateData'))
+  const [appState, setAppState] = useState({
+    loading: false,
+    stateData: null
+  })
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = "https://api.covid19india.org/data.json";
+    axios.get(apiUrl)
+      .then(reponse => {
+        console.log('Server responded');
+        const statewiseData = reponse.data.statewise;
+        // localStorage.setItem('stateData', JSON.stringify(statewiseData))
+        setAppState({ loading: false, stateData: statewiseData });
+      })
+      .catch(error => {
+        setAppState({ loading: false });
+        console.log('Something went wrong');
+      })
+  }, [setAppState]);
+  // console.log(appState)
+  const { loading, stateData } = appState;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Detector
+      render={({ online }) => (
+        <div className={online ? "App" : "Offline-App"}>
+          <div className="container">
+            <h2 className="app-title">Covid-19 India</h2>
+            {stateData &&
+              <>
+                <div className="card confirmed">
+                  <h4>Confirmed</h4>
+                  <h2>
+                    {stateData[0].confirmed}
+                    <small>
+                      (+{stateData[0].deltaconfirmed})
+                    </small>
+                  </h2>
+                </div>
+                <div className="card active">
+                  <h4>Active</h4>
+                  <h2>{stateData[0].active}</h2>
+                </div>
+                <div className="card recovered">
+                  <h4>Recovered</h4>
+                  <h2>
+                    {stateData[0].recovered}
+                    <small>
+                      (+{stateData[0].deltarecovered})
+                    </small>
+                  </h2>
+                </div>
+                <div className="card deceased">
+                  <h4>Deceased</h4>
+                  <h2>
+                    {stateData[0].deaths}
+                    <small>
+                      (+{stateData[0].deltadeaths})
+                    </small>
+                  </h2>
+                </div>
+              </>
+            }
+            {!online &&
+              <span className="offline-text">You are currently offline</span>
+            }
+          </div>
+        </div>        
+      )}
+    />
   );
 }
 
